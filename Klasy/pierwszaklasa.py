3ff404a16b3df2671a1dc5528faecd7cf809d614
ppelka6 <ppelka6@user.noreply.github.com>
2022-05-29 12:57:08 +0200

    added

--- Klasy/pierwszaklasa.py
+++ Klasy/pierwszaklasa.py
@@ -3,9 +3,13 @@
 # pass może być użyte w ciele (a body, które nic nie robi
 # def odejmij(a, b):
 #     pass
-
+# self reprezentuje instancję klasy. Dzięki użyciu self my możemy dostęp do właściwości i metod klasy w Pythonie
+# Tworzenie obiektu realizujemy przez tzw konstruktor - jest to specjalna metoda, która jest wykonywana kiedy tworzymy nasz obiekt
 class Paletka:
-    pass
+    def __init__(self, kolor):
+        self.kolor_obiektu = kolor
+        print(f"Utworzyliśmy obiekt o kolorze: {self.kolor_obiektu}) - ID: {id(self)}")
+
 
 # tworzymy obiekt na podstawie klasy, podajeemy nazwę obiektu (paleetka_a) i wywołujemy konstruktor klasy (Paletka())
 # f-string
@@ -17,6 +21,14 @@ class Paletka:
 # print(f"Hello, my name is {name} nad I am {age} years old")
 
 def testklasy():
-paletka_a = Paletka()
-print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
-print(dir(paletka_a))
+    paletka_a = Paletka("czerwony")
+    paletka_b = Paletka("niebieski")
+    print("*******************")
+    print(f"Obiekt typu {type(paletka_a)} zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_a))
+    print("********************")
+    print(f"Obiekt typu {type(paletka_b)} zawiera od razu pewne właściwości i metody:")
+    print(dir(paletka_b))
+    print("**************************")
+    print(f"Kolor dla paletka_a: {paletka_a.kolor_obiektu}")
+    print(f"Kolor dla paletka_b: {paletka_b.kolor_obiektu}")
